52956ce5d17bc3ec2b2888138e0340a574bd61b2
jinsishuang <1192163737@qq.com>
2019-10-01 10:10:20 +0800

    Add --新增库

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,35 +1,48 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <!-- <swiper>
+      <swiper-item v-for="(item, index) in banner" :key="index">
+        <img :src="item.url" alt="" style="{width: '80px', height: '80px'}">
+      </swiper-item>
+    </swiper> -->
+    <carousel />
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
-  import { getHomeMultidata } from 'network/home'
-  export default {
-    name: 'Home',
-    components: {
-      NavBar
-    },
-    data() {
-      return {
-        list: [],
-        data: []
-      }
-    },
-    created() {
-      getHomeMultidata().then(res => {
-        console.log(res)
-        this.list = res.data;
-      })
-    }
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import { Swiper, SwiperItem } from "components/common/swiper";
+import Carousel from 'components/common/carousel/Carousel';
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    Swiper,
+    SwiperItem,
+    Carousel
+  },
+  data() {
+    return {
+      banner: [],
+      recommends: null
+    };
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.recommends = res.data.recommends;
+      this.banner = res.data.banner;
+    });
   }
+};
 </script>
 
 <style>
-  .home-nav {
-    background-color: var(--color-tint);
-    color: white
-  }
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,8 @@ module.exports = {
                 'components': '@/components',
                 'network': '@/network',
                 'views': '@/views',
-            }
+            },
+            extensions: ['.js', '*', '.css'],  //现在就没问题了
         }
     },
   devServer: {
@@ -29,5 +30,31 @@ module.exports = {
         target: '<other_url>'
       }
     }
-  }
+  },
+  // css: {
+  //   loaderOptions: {
+  //     // 给 sass-loader 传递选项
+  //     sass: {
+  //       // @/ 是 src/ 的别名
+  //       // 所以这里假设你有 `src/variables.sass` 这个文件
+  //       data: `@import "~@/variables.sass"`
+  //     },
+  //     // 默认情况下 `sass` 选项会同时对 `sass` 和 `scss` 语法同时生效
+  //     // 因为 `scss` 语法在内部也是由 sass-loader 处理的
+  //     // 但是在配置 `data` 选项的时候
+  //     // `scss` 语法会要求语句结尾必须有分号，`sass` 则要求必须没有分号
+  //     // 在这种情况下，我们可以使用 `scss` 选项，对 `scss` 语法进行单独配置
+  //     scss: {
+  //       data: `@import "~@/variables.scss";`
+  //     },
+  //     // 给 less-loader 传递 Less.js 相关选项
+  //     less:{
+  //       // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
+  //       // `primary` is global variables fields name
+  //       globalVars: {
+  //         primary: '#fff'
+  //       }
+  //     }
+  //   }
+  // }
 }
